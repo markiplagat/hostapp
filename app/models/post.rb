1e8eaab6c3ea702e2c95528d2fd0822686994b76
markiplagat <markkiplagatkipruttoh@gmail.com>
2021-03-26 11:31:53 +0300

    devise

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { minimum: 4, maximum: 50 }
   validates :description, length: { minimum: 10, maximum:500 }
 
+  belongs_to :user
+
 end
