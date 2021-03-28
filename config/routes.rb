3a908a4f3a6aa6fc176be0e55e5197947f7e5acc
markiplagat <markkiplagatkipruttoh@gmail.com>
2021-03-28 10:35:32 +0300

    delete comment fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root 'home#index'
   devise_for :users
   resources :posts do
     resources :comments
   end
-  root 'home#index'
   get 'posts/new', to: 'posts#new'
   get 'home/about', to: 'home#about'
 end
