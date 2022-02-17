0a79b9f2330332e3d6bd56361515218d2eef16cf
markiplagat <markkiplagatkipruttoh@gmail.com>
2022-02-17 11:34:33 +0300

    Add rich text

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { minimum: 4, maximum: 50 }
   validates :description, length: { minimum: 10, maximum: 500 }
+  has_rich_text :description
 
   belongs_to :user
   has_many :comments, dependent: :destroy
